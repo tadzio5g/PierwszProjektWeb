34976cd0bbccd3f7a49993859a62ef86f38eea4c
Radek <Radek@DESKTOP-IH0U3Q8>
2017-02-16 22:32:02 +0100

    Add project files.

---
+++ PierwszProjektWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PierwszProjektWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PierwszProjektWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PierwszProjektWeb.MvcApplication" Language="C#" %>
